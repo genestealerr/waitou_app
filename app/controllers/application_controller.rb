15b66fe9a715b4bb3b55683ef5e4ee1fdf3ff442
Damien Bouche <bouche.damien.pro@gmail.com>
2015-04-14 19:25:05 +0800

    allow controls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+    before_filter :allow_cross_domain_access
+    def allow_cross_domain_access
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE"
+    end
 end
